01567ec3453f43d32774517bdeb9ad4ed0730afe
Gert Adamson <adamson.gert@hotmail.com>
2019-01-28 22:14:30 +0200

    Tehtävä 2.3 ja 2.4

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -34,35 +34,53 @@ const Course = ({course}) => (
 )
 
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 7,
-        id: 4
-      }
-    ]
-  } 
+  const courses = [
+    {
+      name: 'Half Stack -sovelluskehitys',
+      id: 1,
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'Komponenttien tila',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewaret',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  
+  const courseContents = () => courses.map(course =>
+    <Course key={course.id} course={course} />)
 
   return (
     <div>
-      <Course course={course} />
+      <Header text='Opetusohjelma' />
+      {courseContents()}
     </div>
   )
 }
