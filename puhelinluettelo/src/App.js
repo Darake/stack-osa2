8e32772e580d40160e7618e44db4e749a569fe07
Gert Adamson <adamson.gert@hotmail.com>
2019-01-30 14:55:03 +0200

    Tehtävä 2.8

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '045-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -17,11 +17,12 @@ const App = () => {
       alert(`${newName} on jo luettelossa`)
     }
     else {
-      const personObject = { name: newName }
+      const personObject = { name: newName, number: newNumber }
       setPersons(persons.concat(personObject))
     }
     
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -36,12 +37,21 @@ const App = () => {
           />
         </div>
         <div>
+          numero:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">lisää</button>
         </div>
       </form>
       <h2>Numerot</h2>
       {persons.map((person, key) => 
-        <span key={person.name}>{person.name}<br/></span>)}
+        <span key={person.name}>
+          {person.name} {person.number}<br/>
+        </span>)}
     </div>
   )
 
