22ec969effcf533eaea71f26e38fa9bb70fcde4d
Gert Adamson <adamson.gert@hotmail.com>
2019-01-30 14:22:42 +0200

    Tehtävä 2.6

---
+++ puhelinluettelo/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = { name: newName }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          nimi: 
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">lisää</button>
+        </div>
+      </form>
+      <h2>Numerot</h2>
+      {persons.map((person, key) => 
+        <span key={person.name}>{person.name}<br/></span>)}
+    </div>
+  )
+
+}
+
+export default App
